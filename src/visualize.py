dc3a3198a57fc48e7b8ae0da0e3e28254834a5c6
Lingermania <kristmunduragust@gmail.com>
2019-10-08 17:35:16 +0200

    Decoupled visualization from network module

---
+++ src/visualize.py
@@ -0,0 +1,78 @@
+import plotly.graph_objects as go
+
+
+
+def visualize(G):
+    edge_x = []
+    edge_y = []
+    for edge in G.E:
+        x0, y0 = edge.node_from.x, edge.node_from.y #G.node[edge[0]]['pos']
+        x1, y1 = edge.node_to.x, edge.node_to.y #G.node[edge[1]]['pos']
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    node_x = []
+    node_y = []
+    for node in G.V:
+        x, y = node.x, node.y #G.node[node]['pos']
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            # colorscale options
+            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    node_adjacencies = []
+    node_text = []
+    for node, adjacencies in G.adjacency():
+        node_adjacencies.append(len(adjacencies))
+        node_text.append('# of connections: '+str(len(adjacencies)) + "\nName: " + str(node))
+
+    node_trace.marker.color = node_adjacencies
+    node_trace.text = node_text
+
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+            layout=go.Layout(
+            title='Network of Tourist Attractions in Iceland',
+            titlefont_size=16,
+            showlegend=False,
+            hovermode='closest',
+            margin=dict(b=20,l=5,r=5,t=40),
+            annotations=[ dict(
+                text="Iceland",
+                showarrow=False,
+                xref="paper", yref="paper",
+                x=0.005, y=-0.002 ) ],
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            )
+    fig.show()
