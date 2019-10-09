1b2d7ec76d220a512c4d04d02bb954221e8d42d2
Lingermania <kristmunduragust@gmail.com>
2019-10-09 21:17:27 +0200

    best tour added to evolve info

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -47,8 +47,8 @@ class GA:
             print('Done!')
             print('Population size: {0}'.format(len(self.population.population)))
             print('Average fitness: {0}'.format(self.population.average_fitness))
-            print('Average length of tour: {0}'.format(1/self.population.average_fitness))
-            print('Best tour: {0}'.format(self.best().get_fitness()))
+            print('Average length of tour: {0}'.format(1.0/self.population.average_fitness))
+            print('Best tour: {0}'.format(1.0/self.best().get_fitness()))
             
 
     def best(self):
